2fa35942ac38004d6df48e76aef70b8bda20e637
Roland Sharp <rolandnsharp@gmail.com>
2014-06-28 19:11:27 +1000

    version bump

--- test/index.js
+++ test/index.js
@@ -1,10 +1,9 @@
 var test = require('tape');
 var converter = require('../lib/converter');
-// var resumeJson = {
-//     "name": "test",
-//     "email": "test@test"
-// }
-
+var resumeJson = {
+    "name": "test",
+    "email": "test@test"
+}
 var resumeJson = require('../example/resume.json');
 
 test('Test converter on resume.json.', function(t) {
